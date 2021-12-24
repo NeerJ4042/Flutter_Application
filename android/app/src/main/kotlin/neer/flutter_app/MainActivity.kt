2892a01e8f98adc1308c89393e8993921fb7ef5e
NeerJ4042 <nthummar@upei.ca>
2021-12-24 15:40:04 -0400

    commit 1

---
+++ android/app/src/main/kotlin/neer/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package neer.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello guys chai pelo'),
+        )
+      ),
+      drawer: Drawer(),
+    );
+  }
+
+}
