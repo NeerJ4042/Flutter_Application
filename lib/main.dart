175abe64f0fa4dfb5806834ef6a8a79bad84bd36
NeerJ4042 <nthummar@upei.ca>
2021-12-25 22:51:50 -0400

    Concept of Pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
-import 'Home.dart';
+import 'pages/Home.dart';
+import 'pages/Login.dart';
 
 void main() {
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-       return MaterialApp(home: Home());
+       return MaterialApp(
+         themeMode: ThemeMode.light,
+         theme: ThemeData(primarySwatch: Colors.purple),
+         darkTheme: ThemeData(brightness: Brightness.light),
+         initialRoute: "/home",
+         routes: {
+           "/": (context) => LoginPage(),
+           "/home": (context) => Home(),
+           "/login": (context) => LoginPage()
+         },
+       );
   }
+
+  
 }
